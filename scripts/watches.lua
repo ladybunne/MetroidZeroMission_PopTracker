6349ffa2c01d9214b85dace0276a5379eedee3be
Ladybunne <github@ladybunne.net>
2025-01-08 00:22:36 +1100

    Add future code for renaming Unknown Items based on state

--- scripts/watches.lua
+++ scripts/watches.lua
@@ -10,22 +10,35 @@ end
 local unknown_items_known = {
     ["PlasmaBeam"] = ImageReference:FromPackRelativePath("images/PlasmaBeam.png"),
     ["SpaceJump"] = ImageReference:FromPackRelativePath("images/SpaceJump.png"),
-    ["GravitySuit"] = ImageReference:FromPackRelativePath("images/GravitySuit.png"),
+    ["GravitySuit"] = ImageReference:FromPackRelativePath("images/GravitySuit.png")
 }
 local unknown_items_known_disabled = {
     ["PlasmaBeam"] = ImageReference:FromImageReference(unknown_items_known["PlasmaBeam"], "@disabled"),
     ["SpaceJump"] = ImageReference:FromImageReference(unknown_items_known["SpaceJump"], "@disabled"),
-    ["GravitySuit"] = ImageReference:FromImageReference(unknown_items_known["GravitySuit"], "@disabled"),
+    ["GravitySuit"] = ImageReference:FromImageReference(unknown_items_known["GravitySuit"], "@disabled")
 }
 local unknown_items_unknown = {
     ["PlasmaBeam"] = ImageReference:FromPackRelativePath("images/UnknownPlasmaBeam.png"),
     ["SpaceJump"] = ImageReference:FromPackRelativePath("images/UnknownSpaceJump.png"),
-    ["GravitySuit"] = ImageReference:FromPackRelativePath("images/UnknownGravitySuit.png"),
+    ["GravitySuit"] = ImageReference:FromPackRelativePath("images/UnknownGravitySuit.png")
 }
 local unknown_items_unknown_disabled = {
     ["PlasmaBeam"] = ImageReference:FromImageReference(unknown_items_unknown["PlasmaBeam"], "@disabled"),
     ["SpaceJump"] = ImageReference:FromImageReference(unknown_items_unknown["SpaceJump"], "@disabled"),
-    ["GravitySuit"] = ImageReference:FromImageReference(unknown_items_unknown["GravitySuit"], "@disabled"),
+    ["GravitySuit"] = ImageReference:FromImageReference(unknown_items_unknown["GravitySuit"], "@disabled")
+}
+
+-- You can't change the name of JSON items... apparently... which makes this useless. Unfortunate.
+-- If that ever changes I'll wire these up.
+local unknown_items_names_known = {
+    ["PlasmaBeam"] = "Plasma Beam",
+    ["SpaceJump"] = "Space Jump",
+    ["GravitySuit"] = "Gravity Suit"
+}
+local unknown_items_names_unknown = {
+    ["PlasmaBeam"] = "Unknown Item 1 (Plasma Beam)",
+    ["SpaceJump"] = "Unknown Item 2 (Space Jump)",
+    ["GravitySuit"] = "Unknown Item 3 (Gravity Suit)"
 }
 
 function UpdateUnknownItemIcon(item)
@@ -33,9 +46,11 @@ function UpdateUnknownItemIcon(item)
     if not item_object then
         return
     elseif CanUseUnknownItems() then
+        -- item_object.Name = unknown_items_names_known[item]
         item_object.Icon = item_object.Active and unknown_items_known[item] or unknown_items_known_disabled[item]
         item_object:SetOverlay("")
     else
+        -- item_object.Name = unknown_items_names_unknown[item]
         item_object.Icon = item_object.Active and unknown_items_unknown[item] or unknown_items_unknown_disabled[item]
         item_object:SetOverlay("X")
         item_object:SetOverlayFontSize(14)
